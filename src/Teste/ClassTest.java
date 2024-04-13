7685a594c4d4eb850b890af6d9b1734c68a9da3d
Mateus Martignago Mariot <mateusmartignagomariot@gmail.com>
2024-04-13 10:49:24 -0300

    Pequenas alterações
    -Acho que o principal ponto foi que abri ele no Visual Studio (Criou uns arquivos novos ai)
    -Segundo ponto é que melhorei a classe teste e agora ela ta bonitinha.

--- src/Teste/ClassTest.java
+++ src/Teste/ClassTest.java
@@ -1,21 +1,21 @@
 package Teste;
 
 import Utilities.PasswordUtilities;
-import Utilities.ValidationClass;
+//import Utilities.ValidationClass;
 import Utilities.MatUtilities;
 
 public class ClassTest {
 
 	public static void main(String[] args) {
-		System.out.println(PasswordUtilities.KeyGenerator(8, true, true, false, true));
-		System.out.println(MatUtilities.ApplyDiscValor(100, 110));
-		System.out.println(MatUtilities.IncrementPercVal(100, 200));
-		System.out.println(MatUtilities.SamplingOne(564, 87));
-		System.out.println(MatUtilities.SamplingTwo(563, 431));
-		System.out.println(MatUtilities.ValueDiscWas(1345, 1300));
-		System.out.println(MatUtilities.DeltaVariation(1425, 465));
-		System.out.println(MatUtilities.OriginalValue(1568, 35));
-		System.out.println(MatUtilities.ThreeRule(1345, 1300, 100));
+		System.out.println("Gerador de senha: "+PasswordUtilities.KeyGenerator(8, true, true, false, true));
+		System.out.println("Aplicar % de desconto em um valor: "+MatUtilities.ApplyDiscValor(100, 30));
+		System.out.println("Incrementear % a um valor: "+MatUtilities.IncrementPercVal(100, 200));
+		System.out.println("Quanto X% representa de Y: "+MatUtilities.SamplingOne(564, 87));
+		System.out.println("Quanto Y representa de Y: "+String.format("%.2f", MatUtilities.SamplingTwo(563, 431)));
+		System.out.println("Valor era A e paguei B, quantos % foi o desconto? "+String.format("%.2f", MatUtilities.ValueDiscWas(1345, 1300)));
+		System.out.println("Diferença em % entre dois valores: "+String.format("%.2f", MatUtilities.DeltaVariation(564, 87)));
+		System.out.println("Qual o valor original? "+String.format("%.2f", MatUtilities.OriginalValue(1568, 35)));
+		System.out.println("Regra de três: "+String.format("%.2f", MatUtilities.ThreeRule(1345, 1300, 100)));
 	}
 
 }
