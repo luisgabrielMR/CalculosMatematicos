efcfe79feb403a5a728ddba6b0b7e7f3fcf62cdf
Mateus Martignago Mariot <mateusmartignagomariot@gmail.com>
2024-04-16 21:10:33 -0300

    Criação das funções de validação
    Foi criado as classes de validação e comentado como fazer uso das mesmas.

--- src/Utilities/MatUtilities.java
+++ src/Utilities/MatUtilities.java
@@ -1,40 +1,39 @@
 package Utilities;
 
 public class MatUtilities {
-	//Nenhum dos valores pode ser == 0;
 	public static double ApplyDiscValor(double a, double b){
 		//Aplicar desconto % em um valor:
-		//Classe utilitária de validação criada;
+		//Classe utilitária de validação criada;BbiggerthanHundred
 		return a - (a*(b/100));
 	}
 	public static double IncrementPercVal(double a, double b) {
 		//Incrementar % a um valor:
-		//Não precisa de classe utilitária para validação;
+		//Classe utilitária de validação criada;BbiggerthanHundred
 		return a + (a*(b/100));
 	}
 	public static double SamplingOne(double a, double b) {
 		//Amostragem - Quanto X% representa de Y:
-		//O valor de b não pode ser maior do que de 100%;
+		//O valor de b não pode ser maior do que de 100%;BbiggerthanHundred
 		return (a*b)/100;
 	}
 	public static double SamplingTwo(double a, double b) {
 		//Amostragem 2 - Quanto X representa de Y:
-		//O valor de "b" não pode ser maior do que de "a";
+		//O valor de "b" não pode ser maior do que de "a"; BbiggherthanA
 		return (b*100)/a;
 	}
 	public static double ValueDiscWas(double a, double b) {
 		//Valor era A e paguei B, qual foi o desconto%?
-		//Valor de "b" não pode ser maior do que "a";
+		//Valor de "b" não pode ser maior do que "a"; BbiggherthanA
 		return ((a-b)/a)*100;
 	}
 	public static double DeltaVariation(double a, double b) {
 		//Variação Delta(%) - diferença % entre valores
-		//Tecnicamente não precisa de validação por classe utilitária, validar essa info;
+		//"b" não pode ser maior do que "a"; BbiggherthanA
 		return 100-((b*100)/a);
 	}
 	public static double OriginalValue(double a, double b) {
 		//Qual o valor original?
-		//O valor de "b" não pode ser maior do que 100;
+		//O valor de "b" não pode ser maior do que 100; BbiggerthanHundred
 		return (a * 100 / (100 - b));
 	}
 	public static double ThreeRule(double a, double b, double r1) {
