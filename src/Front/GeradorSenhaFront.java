d7741395b0d01ec362b0bd4c662fdf0423d4266e
Karen Bialescki Stackoski <stackoskikaren@gmail.com>
2024-04-23 21:41:33 -0300

    Senha iniciado

--- src/Front/GeradorSenhaFront.java
+++ src/Front/GeradorSenhaFront.java
@@ -1,16 +1,37 @@
 package Front;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
 
 public class GeradorSenhaFront extends JFrame{
+	private JLabel lbMaiuscula;
+	private JLabel lbMinuscula;
+	private JLabel lbNumeros;
+	private JLabel lbSimbolos;
+	private JLabel lbTamanho;
+	private JLabel lbSenha;
+	private JComboBox<Boolean> cbMaiuscula;
+	private JComboBox<Boolean> cbMinuscula;
+	private JComboBox<Boolean> cbNumeros;
+	private JComboBox<Boolean> cbSimbolos;
+	private JSpinner spTamanho;
+	private JButton btnGerar;
+	private JTextField txtSenha;
+	
 	public GeradorSenhaFront() {
 		setSize(500, 300);
         setLocationRelativeTo(null);
+        setLayout(null);
         setTitle("Gerador de Senha");
-
-        // Adicionar os componentes desejados à nova janela
-        JLabel label = new JLabel("Nova janela aberta!");
-        getContentPane().add(label);
+        createComponents();
+        setVisible(true);
+	}
+	
+	public void createComponents() {
+		
 	}
 }
